a09cc15dc0306c2ff0660148619730e643c4be79
sartra <strenshaw@gmail.com>
2017-06-28 13:46:11 -0400

    added projects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,13 +1,126 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+ var work = {
+    "jobs": [{
+        "employer": "Trigyn Tech",
+        "title": "Design Consultant at the United Nations",
+        "location": "New York City",
+        "dates": "December 2016 - May 2017",
+        "description": "Designing infographics (Illustrator) and laying out reports(InDesign); Assisting with the Office Of Administration of Justice's new website - redesigned with a Bootstrap framework"
+    }, {
+        "employer": "Chess NYC, Inc",
+        "title": "Design and Marketing Associate",
+        "location": "New York City",
+        "dates": "February 2015 - November 2016",
+        "description": "Designed promotional materials such as posters, fliers, and banner ads (digital and print); updating Joomla CMS; Redesigned website with a Bootstrap framework; Email marketing using Constant Contact"
+    }, {
+        "employer": "Kokrobitey Institute",
+        "title": "Designer",
+        "location": "Accra, Ghana",
+        "dates": "January 2013 - January 2014",
+        "description": "Lead the marketing campaign for the Ghana School Bag initiative; Designed billboards, posters, fliers; Researched and designed educational materials that were distributed with the backpacks to schools throughout Ghana"
+    }, {
+        "employer": "Sartra Design",
+        "title": "Designer",
+        "location": "New York City",
+        "dates": "August 2010 - June 2017",
+        "description": "Freelance designer working with companies and individuals throughout NYC and remotely; Creation of posters, fliers, booklets and various promotional materials; Assistance with websites for artists and creation of digital archives; Digital marketing"
+    }]
+}
+
+var education = {
+    "schools": [{
+        "name": "Concordia University",
+        "dates": "2008-2010",
+        "location": "Montreal, QC",
+        "degree": "Ph.D. Candidate",
+        "majors": ["Art, Design & Communication Studies"]
+    },
+    {
+        "name": "Rhode Island School of Design",
+        "dates": "2005-2007",
+        "location": "Providence, Rhode Island",
+        "degree": "Master of Fine Arts",
+        "majors": ["Digital Media"]
+    }, 
+    {
+        "name": "University of Illinois at Urbana Champaign",
+        "dates": "2000-2004",
+        "location": "Urbana, Illinois",
+        "degree": "Bachelor of Fine Arts",
+        "majors": ["Fine Art and Design"]
+    }, 
+    {
+        "name": "Goldsmiths College, University of London",
+        "dates": "2002-2003",
+        "location": "London, United Kingdom",
+        "degree": "Bachelor of the Arts",
+        "majors": ["Art & Design Theory"]
+    }],
+    "onlineCourses": [{
+        "school": "Udacity",
+        "title": "Front-End Web Development Nanodegree",
+        "date": "May 2017",
+        "url": "https://www.udacity.com"
+    }, {
+        "school": "Google",
+        "title": "Certified in Google Analytics",
+        "date": "November 2016",
+        "url": "https://www.Google.com"
+    }]
+}
+
+var projects = {
+    "projects": [{
+        "title": "HTML5 Canvas Game",
+        "dates": "July 2017",
+        "description": "Created an online game using HTML5 Canvas as part of Udacity's Front-End Web Developer Nanodegree.",
+        "images": ["https://unsplash.it/300/200"],
+        "url": "http://www.sartradesign.com"
+    }, {
+        "title": "Online Portfolio",
+        "dates": "October 2014",
+        "description": "Created an online portfolio of work as part of Udacity's Front-End Web Developer Nanodegree.",
+        "images": ["https://unsplash.it/300/200"],
+        "url": "http://www.sartradesign.com"
+    }, {
+        "title": "Web Development Wiki",
+        "dates": "June 2017 - August 2017",
+        "description": "Created an online wiki for Udacity's Web Development Course.",
+        "images": ["https://unsplash.it/300/200"],
+        "url": "http://www.sartradesign.com"
+    }]
+};
+
+projects.display = function () {
+   //start loop
+   projects.projects.forEach(function(project) {
+      
+      $('#projects').append(HTMLprojectStart);
+
+       project.images.forEach(function(image) {
+        var formattedProjectImage = HTMLprojectImage.replace('%data%', project.images);
+        $('.project-entry:last').append(formattedProjectImage);
+      });
+
+
+      var formattedProjectTitle = HTMLprojectTitle.replace('%data%', project.title).replace('#', project.url);
+      var formattedProjectDates = HTMLprojectDates.replace('%data%', project.dates);
+      var formattedProjectDescription = HTMLprojectDescription.replace('%data%', project.description);
+
+      // attach to the class of div reated in HTMLprojectStart
+      $('.project-entry:last').append(formattedProjectTitle, formattedProjectDates, formattedProjectDescription, '<br>');
+
+     
+   });
+}
+
+projects.display();
 
 
 // $("#main").append("Sarah T Renshaw");
 
  var name = "S.T.Renshaw";
  var role = "Designer and Front End Developer";
- var skills = ['HTML/CSS', 'JS', 'Responsive Design'];
+ var skills = ['HTML','CSS', 'JavaScript', 'Responsive Design'];
  var email = "strenshaw@gmail.com";
  var pic = "images/asm.jpg";  // change to me.jpg
  var message = ""; 
@@ -31,6 +144,8 @@ This is empty on purpose! Your code to build the resume will go here.
     'message': "Creative Design and Code", 
     'skills': skills
  }
+
+
 
  var formattedName = HTMLheaderName.replace("%data%", name);
 
@@ -82,41 +197,7 @@ for (var i=0; i<skills.length; i++){
 }
 
 
- // var work = {
- //    employer: 'Trigyn Tech',
- //    title: 'Design Consultant at the United Nations',
- //    dates: 'December 2016 - May 2017',
- //    description: 'Designing infographics and laying out reports; Assisting with the Office Of Administration of Justice\'s new website - redesigned with a Bootstrap framework',
- //    location: 'New York City'
- // }
 
- var work = {
-    "jobs": [{
-        "employer": "Trigyn Tech",
-        "title": "Design Consultant at the United Nations",
-        "location": "New York City",
-        "dates": "December 2016 - May 2017",
-        "description": "Designing infographics (Illustrator) and laying out reports(InDesign); Assisting with the Office Of Administration of Justice\'s new website - redesigned with a Bootstrap framework"
-    }, {
-        "employer": "Chess NYC, Inc",
-        "title": "Design and Marketing Associate",
-        "location": "New York City",
-        "dates": "February 2015 - November 2016",
-        "description": "Designed promotional materials such as posters, fliers, and banner ads (digital and print); updating Joomla CMS; Redesigned website with a Bootstrap framework; Email marketing using Constant Contact"
-    }, {
-        "employer": "Kokrobitey Institute",
-        "title": "Designer",
-        "location": "Accra, Ghana",
-        "dates": "January 2013 - January 2014",
-        "description": "Lead the marketing campaign for the Ghana School Bag initiative; Designed billboards, posters, fliers; Researched and designed educational materials that were distributed with the backpacks to schools throughout Ghana"
-    }, {
-        "employer": "Sartra Design",
-        "title": "Designer",
-        "location": "New York City",
-        "dates": "August 2010 - June 2017",
-        "description": "Freelance designer working with companies and individuals throughout NYC and remotely; Creation of posters, fliers, booklets and various promotional materials; Assistance with websites for artists and creation of digital archives; Digital marketing"
-    }]
-};
 
 work.display = function() {
 
@@ -139,47 +220,7 @@ work.display = function() {
 work.display();
 
 
-var education = {
-    "schools": [{
-        "name": "Concordia University",
-        "dates": "2008-2010",
-        "location": "Montreal, QC",
-        "degree": "Ph.D. Candidate",
-        "majors": ["Art, Design & Communication Studies"]
-    },
-    {
-        "name": "Rhode Island School of Design",
-        "dates": "2005-2007",
-        "location": "Providence, Rhode Island",
-        "degree": "Master of Fine Arts",
-        "majors": ["Digital Media"]
-    }, 
-    {
-        "name": "University of Illinois at Urbana Champaign",
-        "dates": "2000-2004",
-        "location": "Urbana, Illinois",
-        "degree": "Bachelor of Fine Arts",
-        "majors": ["Fine Art and Design"]
-    }, 
-    {
-        "name": "Goldsmiths College, University of London",
-        "dates": "2002-2003",
-        "location": "London, United Kingdom",
-        "degree": "Bachelor of the Arts",
-        "majors": ["Art & Design Theory"]
-    }],
-    "onlineCourses": [{
-        "school": "Udacity",
-        "title": "Front-End Web Development Nanodegree",
-        "date": "May 2017",
-        "url": "https://www.udacity.com"
-    }, {
-        "school": "Google",
-        "title": "Certified in Google Analytics",
-        "date": "November 2016",
-        "url": "https://www.Google.com"
-    }],
-};
+
 
 
 education.display = function() {
@@ -195,8 +236,9 @@ education.display = function() {
         var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", school.location);
         var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", school.majors);
 
+        var formattedSchoolName_Degree = formattedSchoolName + formattedSchoolDegree;
 
-       $(".education-entry:last").append(formattedSchoolName, formattedSchoolDegree, formattedschoolDates, formattedSchoolMajor, formattedSchoolLocation);
+       $(".education-entry:last").append(formattedSchoolName_Degree, formattedschoolDates, formattedSchoolMajor, formattedSchoolLocation);
         
         // $(".education-entry").append(formattedSchoolName, formattedSchoolDegree, formattedschoolDates, formattedSchoolMajor, formattedSchoolLocation);
     });
@@ -211,11 +253,11 @@ education.display = function() {
           $("#education").append(HTMLschoolStart);
           
 
-
         var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", s.title);
         var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", s.school);
         var formattedOnlineDates = HTMLonlineDates.replace("%data%", s.date);
-        var formattedOnlineURL = HTMLonlineURL.replace("%data%", s.url);
+        var formattedOnlineURL = HTMLonlineURL.replace("%data%", s.url).replace("#", s.url);
+
 
         var formattedOnlineTitle_School = formattedOnlineTitle +formattedOnlineSchool;
 
